3e838912570cbdab004f572f9ff6a0c8be0a3e07
imogencs <Imogen.ics1@gmail.com>
2021-05-02 15:52:43 -0400

    Create data_generation.js

---
+++ data_generation.js
@@ -0,0 +1,72 @@
+function random_in_range(range_start = 0, range_end = 1) {
+    return Math.random() * (range_end - range_start) + range_start
+}
+
+function generate_hourly_pops(pop_range_start = 0, pop_range_end = 1, type = null, n = 24) {
+    // start with a totally random pop between pop_range_start and pop_range_end
+    let pops = []
+    pops[0] = Math.round(random_in_range(pop_range_start, pop_range_end) * 20) / 20
+
+    // then decide randomly ascending or descending pop
+    let isAscending = !!Math.round(Math.random())
+
+    // 90% chance to continue ascending or descending based on that decision
+    // switch to descending once hit 90%+
+    // change pop by increments of 0-20%
+
+    let decision_boundary = .8
+    let max_increment = (pop_range_end - pop_range_start) / 4
+
+    let decision = Math.random()
+
+    for (let i = 1; i < n; i++) {
+        decision = Math.random()
+        increment = random_in_range(0, max_increment)
+
+        if ((decision >= decision_boundary && isAscending) || (decision < decision_boundary && !isAscending)) {
+            // ascending
+            pops[i] = pops[i - 1] + increment
+        } else {
+            // descending
+            pops[i] = pops[i - 1] - increment
+        }
+
+        if (pops[i] >= pop_range_end) { // reached max pop
+            isAscending = !isAscending
+            if (pops[i] > pop_range_end) {
+                pops[i] = pop_range_end
+            }
+        }
+        if (pops[i] <= pop_range_start) { // reached min pop
+            isAscending = !isAscending
+            if (pops[i] < pop_range_start) {
+                pops[i] = pop_range_start
+            }
+        }
+        pops[i] = Math.round(pops[i] * 20) / 20
+    }
+    return pops
+}
+
+
+function generate_hourly_amounts(pops) {
+    // inches of percipitation
+    let amounts = []
+    let min_amount = 0
+    let max_amount = .6
+    let no_rain_boundary = .1
+
+    for (let i = 0; i < pops.length; i++) {
+        // if (pops[i] <= no_rain_boundary) {
+        //     amounts[i] = min_amount
+        // } else {
+            amounts[i] = Math.round(random_in_range(min_amount, max_amount) * pops[i] * 20) / 20
+        // }
+    }
+    return amounts
+}
+
+
+// pops = generate_hourly_pops()
+// console.log(pops)
+// console.log(generate_hourly_amounts(pops))
